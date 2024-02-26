7be38425034b8d627e51e8e47de0a49cdbc425af
Om Yadav <omydv3@gmail.com>
2024-02-26 19:12:07 +0530

    cards image path update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import MainNews from "./components/MainNews";
 import WhatsNew from "./components/WhatsNew";
 import ProductCard from "./components/ProductCard";
+import card1 from "/card1.jpg";
+import card2 from "/card2.jpg";
+import card3 from "/card3.jpg";
 
 const App = () => {
   return (
@@ -14,16 +17,19 @@ const App = () => {
       </div>
       <div className="flex my-6 justify-center gap-4">
         <ProductCard
+          url= {card1}
           ProductNumber="01 "
           ProductName="Reviving Retro PCs"
           Productdetails="What happens when old PCs are given modern upgrades?"
         />
         <ProductCard
+          url= {card2}
           ProductNumber="02 "
           ProductName="Top 10 Laptops of 2022"
           Productdetails="Our best picks for various needs and budgets."
         />
         <ProductCard
+        url= {card3}
           ProductNumber="03 "
           ProductName="The Growth of Gaming"
           Productdetails=" How the pandemic has sparked fresh opportunities."
